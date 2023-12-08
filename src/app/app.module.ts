3d6017689402482b5c35f96631dd85219fb7cf4a
Trisha May Gonzales <126333823+gonzalestrisha@users.noreply.github.com>
2023-12-08 21:04:01 +0800

    updated app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,16 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore'
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirebaseApp(() => initializeApp({
+      apiKey: "AIzaSyCtcBsWwWZjhI2bd9_ywJ62XGz2ZtNQpj0",
+      authDomain: "project-sap-3ec10.firebaseapp.com",
+      databaseURL: "https://project-sap-3ec10-default-rtdb.asia-southeast1.firebasedatabase.app",
+      projectId: "project-sap-3ec10",
+      storageBucket: "project-sap-3ec10.appspot.com",
+      messagingSenderId: "1008196345005",
+      appId: "1:1008196345005:web:2a4de4c8e112d46042fa24",
+      measurementId: "G-9W51KV5VL4"
+    })),
     provideFirestore(() => getFirestore())
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
