f671c8c5b10c9f1201d8991cda0b84e89dbf5501
Trisha May Gonzales <126333823+gonzalestrisha@users.noreply.github.com>
2023-12-12 02:30:32 +0800

    Dashboard UI

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -3,37 +3,13 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  templateUrl: 'login.page.html',
+  styleUrls: ['login.page.scss'],
 })
 export class LoginPage {
-  // validationUserMessage = {
-  //   username: [
-  //     {type:"required", message:"Please enter your username."},
-  //     {type:"string", message:"The username you entered is incorrect."}
-  //   ],
-  //   password: [
-  //     {type:"required", message:"Please enter your password."},
-  //     {type:"minlength", message:"The password must be at least 6 characters."}
-  //   ]
-  // }
 
-  // validationFormUser!: FormGroup;
 
   constructor() { 
   }
-
-  // ngOnInit() {
-  //   this.validationFormUser = this.formbuilder.group({
-  //     password: new FormControl('', Validators.compose([
-  //       Validators.required,
-  //       Validators.minLength(6)
-  //     ]))
-  //   })
-  // }
-
-  // LoginUser(value:any) {
-  //   console.log("Am logged in");
-  // }
 
 }
