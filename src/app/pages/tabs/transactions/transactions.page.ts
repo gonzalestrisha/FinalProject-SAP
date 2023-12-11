b15fd4dbc41be82f78f9f4a385f0c57798001d86
JaharaKate <21-01880@g.batstate-u.edu.ph>
2023-12-12 01:03:00 +0800

    Transaction(database)

--- src/app/pages/tabs/transactions/transactions.page.ts
+++ src/app/pages/tabs/transactions/transactions.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService, Transaction } from 'src/app/services/data.service';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-transactions',
@@ -6,10 +9,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transactions.page.scss'],
 })
 export class TransactionsPage implements OnInit {
+ transactions: Transaction[] = [];
+  public alertButtons = [
+    {
+      text: 'Cancel',
+      role: 'cancel',
+    },
+    {
+      text: 'Delete',
+      role: 'confirm',
+      handler: () => {
+        console.log('Alert confirmed');
+      },
+    },
+  ];
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  constructor(private dataService: DataService, private alertController: AlertController) {
+    this.dataService.getTransactions().subscribe(res => {
+      this.transactions = res;
+    })
   }
 
-}
+  async deleteTransaction(transactions: Transaction) {
+    const alert = await this.alertController.create({
+      header: 'Delete Transaction',
+      message: 'Are you sure you want to delete this transaction?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Delete',
+          role: 'destructive',
+          handler: () => {
+            this.dataService.deleteTransaction(transactions);
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+
+} 
