db2286fb98d7b5fdfda571b6a708ebb0aae46859
AJ <126333823+gonzalestrisha@users.noreply.github.com>
2023-12-11 19:07:38 +0800

    Update UI of Request Maintenance Page
    Will add Create database functionalities later

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from 'src/app/services/data.service';
 // import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -21,10 +20,7 @@ export class LoginPage {
 
   // validationFormUser!: FormGroup;
 
-  constructor(private dataService: DataService) { 
-    this.dataService.getMaintenanceReqs().subscribe(res => {
-      console.log(res)
-    })
+  constructor() { 
   }
 
   // ngOnInit() {
